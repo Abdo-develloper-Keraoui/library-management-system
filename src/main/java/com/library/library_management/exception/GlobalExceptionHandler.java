6411fa7de1de1e92ab10ca06e0ecfcbba8a32767
Abdottawab KERAOUI <abdottawabkeraoui@gmail.com>
2026-02-21 10:02:30 +0000

    Add exception handler for MethodArgumentNotValidException to improve validation error responses

--- src/main/java/com/library/library_management/exception/GlobalExceptionHandler.java
+++ src/main/java/com/library/library_management/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package com.library.library_management.exception;
 import com.library.library_management.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,6 +28,21 @@ public class GlobalExceptionHandler {
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),//400 error message
                 ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                message,
                 LocalDateTime.now()
         );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
